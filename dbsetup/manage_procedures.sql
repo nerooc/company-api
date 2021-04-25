3a1bad31261a5cfaac464d27a67fe9f3a920df3c
Tomasz Gajda <tomasz_gajda@outlook.com>
2021-04-25 23:38:48 +0200

    employee table creation

--- dbsetup/manage_procedures.sql
+++ dbsetup/manage_procedures.sql
@@ -1,1 +1,11 @@
-CREATE TABLE Employees ();
+use cmapiDB
+go  
+
+CREATE TABLE Employees (
+        id int NOT NULL UNIQUE,
+		level hierarchyid NOT NULL UNIQUE,
+		firstName nvarchar(30) NOT NULL,
+        lastName nvarchar(30) NOT NULL,
+		position nvarchar(30) NOT NULL,
+		salary int NOT NULL
+	);
