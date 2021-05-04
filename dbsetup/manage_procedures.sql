cbd3dbdd69c206079308fd972022eecd22fcbf93
Tomasz Gajda <tomasz_gajda@outlook.com>
2021-05-04 22:16:13 +0200

    create addEmployee procedure

--- dbsetup/manage_procedures.sql
+++ dbsetup/manage_procedures.sql
@@ -1,7 +1,7 @@
 use cmapiDB
 go  
 
-CREATE TABLE Employees (
+CREATE TABLE Employee (
         id int NOT NULL UNIQUE,
 		level hierarchyid NOT NULL UNIQUE,
 		firstName nvarchar(30) NOT NULL,
@@ -9,3 +9,10 @@ CREATE TABLE Employees (
 		position nvarchar(30) NOT NULL,
 		salary int NOT NULL
 	);
+
+CREATE PROCEDURE AddEmployee 
+@level as varchar(1000), @firstName as varchar(1000), @lastName as varchar(1000), @position as varchar(1000), @salary as Int
+AS BEGIN
+    INSERT Employee VALUES ( hierarchyid::Parse(@level), @firstName, @lastName, @position, @salary );
+END
+go
