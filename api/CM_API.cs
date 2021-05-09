bd8a9b15519f004c4a77e58e34f06f83cc131a4e
Tomasz Gajda <tomasz_gajda@outlook.com>
2021-05-09 14:46:29 +0200

    create employee class in the API

--- api/CM_API.cs
+++ api/CM_API.cs
@@ -4,4 +4,29 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 
-namespace companyApi{}
+namespace companyApi{
+    // Klasa Employee
+    internal class Employee{
+        public int id;
+        public SqlHierarchyId level;
+        public string firstName;
+        public string lastName;
+        public string position;
+        public int salary;
+        // Konstruktor klasy Employee
+        public Employee(int id, SqlHierarchyId level, string firstName, string lastName, string position, int salary){
+            this.id = id;
+            this.level = level;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.position = position;
+            this.salary = salary;
+        }
+
+        public void print(){
+            Console.WriteLine($"#{id}: {level} - {firstName} {lastName} - {position} - {salary}");
+        }
+    }
+
+
+}
