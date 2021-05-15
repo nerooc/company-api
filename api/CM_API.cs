b394ca26667253eb7ea1e45a3b242ca7050a0da3
Tomasz Gajda <tomasz_gajda@outlook.com>
2021-05-15 10:06:37 +0200

    updated company class - adding and removing employees

--- api/CM_API.cs
+++ api/CM_API.cs
@@ -37,6 +37,69 @@ namespace companyApi{
                 apiConnection = new SqlConnection(connection);
                 apiConnection.Open();
             }   
+
+            public void addEmployee(int id, string level, string firstName, string lastName, string position, int salary){
+                SqlCommand addCommand = new SqlCommand("AddEmployee", apiConnection){
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                addCommand.Parameters.Add("@id", SqlDbType.Int).Value = id;
+                addCommand.Parameters.Add("@level", SqlDbType.VarChar).Value = level;
+                addCommand.Parameters.Add("@firstName", SqlDbType.VarChar).Value = firstName;
+                addCommand.Parameters.Add("@lastName", SqlDbType.VarChar).Value = lastName;
+                addCommand.Parameters.Add("@position", SqlDbType.VarChar).Value = position;
+                addCommand.Parameters.Add("@salary", SqlDbType.Int).Value = salary;
+
+                addCommand.ExecuteNonQuery();
+            }
+
+            public void removeEmployeeById(int id){
+                SqlCommand removeCommand = new SqlCommand("RemoveEmployeeById", apiConnection){
+                    CommandType = CommandType.StoredProcedure
+                };
+                
+                removeCommand.Parameters.Add("@id", SqlDbType.Int).Value = id;
+
+                removeCommand.ExecuteNonQuery();
+            }
+
+            public void removeEmployeeByFirstName(string firstName){
+                SqlCommand removeCommand = new SqlCommand("RemoveEmployeeByFirstName", apiConnection){
+                    CommandType = CommandType.StoredProcedure
+                };
+                
+                removeCommand.Parameters.Add("@firstName", SqlDbType.VarChar).Value = firstName;
+
+                removeCommand.ExecuteNonQuery();
+            }
+
+            public void removeEmployeeByLastName(string lastName){
+                SqlCommand removeCommand = new SqlCommand("RemoveEmployeeByLastName", apiConnection){
+                    CommandType = CommandType.StoredProcedure
+                };
+                
+                removeCommand.Parameters.Add("@lastName", SqlDbType.VarChar).Value = lastName;
+
+                removeCommand.ExecuteNonQuery();
+            }
+
+            public void removeEmployeeByLevel(string level){
+                SqlCommand removeCommand = new SqlCommand("RemoveEmployeeByLevel", apiConnection){
+                    CommandType = CommandType.StoredProcedure
+                };
+                
+                removeCommand.Parameters.Add("@level", SqlDbType.VarChar).Value = level;
+
+                removeCommand.ExecuteNonQuery();
+            }
+
+            public void removeAllEmployees(){
+                SqlCommand removeCommand = new SqlCommand("RemoveAllEmployees", apiConnection){
+                    CommandType = CommandType.StoredProcedure
+                };
+                
+                removeCommand.ExecuteNonQuery();
+            }
         }
     }
 
